f2970afa6e992d9d4f2d0b416e6f05e9cd2a368c
plveilleux <plveilleux@tritondigital.com>
2016-09-28 09:31:17 -0400

    Show arguments

--- src/main/java/app/HelloApp.java
+++ src/main/java/app/HelloApp.java
@@ -1,9 +1,12 @@
 package app;
 
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
 import static spark.Spark.get;
 
 public class HelloApp {
     public static void main(String[] args) {
+        System.out.println("Args = " + stream(args).collect(joining(", ")));
         get("/hello", (request, response) -> "Hello World");
         get("/hello/:name",
                 (request, response) -> "Hello: " + request.params(":name")
